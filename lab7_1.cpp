8ed7c32002d29b76f6ee07d8d5b3fb65147cf533
PICHAYOOT HUNCHAINAO <pichayoothunchainao@gmail.com>
2021-11-30 15:11:35 +0700

    Complete7.1

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -34,8 +34,15 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string x;
+    cout << "Input text: ";
+	cin >> x;
+    cout << "Reversed text: "<<func1(x)<<endl;
+	if(func3(x)==func3(func1(x))){
+		cout << "Palindrome: Yes";
+	}else{
+		cout << "Palindrome: No";
+	}
+
     return 0;
 }
