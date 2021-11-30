3dd3ae35d8af14ac323ca03a0d775d7a55996f1e
PICHAYOOT HUNCHAINAO <pichayoothunchainao@gmail.com>
2021-11-30 16:46:50 +0700

    Completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,39 @@
 
 using namespace std;
 
+int adiff(int a,int b){
+
+  if(a>360 || a<-360){
+    a=a%360;
+  }
+  if(b>360 || b<-360){
+    b=b%360;
+  }
+
+
+int c;
+  if(a>b){
+    c=a-b;
+  }else{
+    c=b-a;
+  }
+  if(c>180){
+    c=c-360;
+    if(c<0){
+      c=-c;
+    }
+  }
+  return c;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  int a,b;
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
+  cout << adiff(60,244)<<endl;
 }
